67509d7551e707f9034781ce0cc6fd3966be04a3
awslabtest2026@gmail.com <B0308848@airtel.com>
2026-05-09 01:05:01 +0530

    first commit

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ print("Hello, World!")
 
 
 
-def add(a, b):
+#def add(a, b):
+#    return a + b
+
+#if __name__ == "__main__":
+#    result = add(5, 7)
+print("The result of adding 5 and 7 is: {result}")
+
+
+
+
